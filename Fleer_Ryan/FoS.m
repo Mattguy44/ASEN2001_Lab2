1fe08b90e633870b10a8f7dfb2be8070c24eb266
etfl2019 <32463847+etfl2019@users.noreply.github.com>
2017-10-22 02:06:49 -0600

    Add files via upload

---
+++ Fleer_Ryan/FoS.m
@@ -0,0 +1,10 @@
+function [] = FoS( Max_load, Uncertainty )
+prompt = 'Enter desired failure probability: '
+P_dsr = input(prompt);
+F_dsr = icdf('normal', P_dsr, Max_load, Uncertainty);
+n = (Max_load - F_dsr)/Uncertainty;
+FOS = 1/(1-n*(Uncertainty/Max_load));
+F_design = Max_load/FOS;
+
+end
+
